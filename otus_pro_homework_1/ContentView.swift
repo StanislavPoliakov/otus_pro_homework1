dcccf6258f1da79f6824447e2d5218ada126bc05
Stanislav Poliakov <polyakov.s.de@sberbank.ru>
2024-09-09 08:43:37 +0300

    Initial Commit

---
+++ otus_pro_homework_1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  otus_pro_homework_1
+//
+//  Created by Поляков Станислав Денисович on 09.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ otus_pro_homework_1/otus_pro_homework_1App.swift
@@ -0,0 +1,17 @@
+//
+//  otus_pro_homework_1App.swift
+//  otus_pro_homework_1
+//
+//  Created by Поляков Станислав Денисович on 09.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct otus_pro_homework_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
